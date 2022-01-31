d90f938077c4e3e67add5de7af29190e8e7da786
annahmercer <anmer@mit.edu>
2022-01-30 23:12:04 -0500

    Inserted bar charts, updated data

--- js/main.js
+++ js/main.js
@@ -5,5 +5,10 @@ let winner_titles;
 d3.csv("data/15.062 Chopped Data.csv", (d)=> {return d}).then(
     function(data) {
         console.log(data)
-        //winner_titles = new BarChart('winner_titles', data)
+        //console.log(data =>data.)
+        winner_titles = new BarChart("top_winner_titles", data, "contestant4_title")
+        winner_locations = new BarChart("top_winner_locations", data, "contestant4_location")
+        winner_genders = new BarChart("winner_genders", data, "contestant4_gender")
     });
+
+
