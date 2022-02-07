8a4e1ff90eb961465a6e4d6515227282746f6afc
annahmercer <anmer@mit.edu>
2022-02-06 20:12:23 -0500

    Winners section

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ d3.csv("data/15.062 Chopped Data.csv", (d)=> {return d}).then(
     function(data) {
         console.log(data)
         //console.log(data =>data.)
-        winner_titles = new BarChart("top_winner_titles", data, "contestant4_title")
-        winner_locations = new BarChart("top_winner_locations", data, "contestant4_location")
-        winner_genders = new BarChart("winner_genders", data, "contestant4_gender")
+        winner_titles = new BarChart("top_winner_titles", data, "contestant4_title", "Winner Job Titles")
+        winner_locations = new BarChart("top_winner_locations", data, "contestant4_location", "Winner Locations")
+        winner_genders = new BarChart("winner_genders", data, "contestant4_gender", "Winner Gender Breakdown")
     });
 
 
